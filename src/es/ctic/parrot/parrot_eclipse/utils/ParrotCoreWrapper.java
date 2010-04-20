c70c104cb3f8036891f2c970f66be50109b04278
Nacho Barrientos <nacho@criptonita.com>
2010-04-20 17:54:36 +0200

    Parrot core link

--- src/es/ctic/parrot/parrot_eclipse/utils/ParrotCoreWrapper.java
+++ src/es/ctic/parrot/parrot_eclipse/utils/ParrotCoreWrapper.java
@@ -1,37 +1,48 @@
 package es.ctic.parrot.parrot_eclipse.utils;
 
-//import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-/*
+
 import es.ctic.parrot.appserv.ParrotAppServ;
 import es.ctic.parrot.reader.DocumentReader;
+import es.ctic.parrot.reader.StringInput;
 import es.ctic.parrot.reader.jena.JenaOWLReader;
 import es.ctic.parrot.reader.naiverifxml.RifXmlReader;
-*/
+import es.ctic.parrot.DocumentaryProject;
+
 
 public class ParrotCoreWrapper {
 	
-	/*
 	private ParrotAppServ app;
 	private InputStream template;
-	*/
 	
 	public ParrotCoreWrapper() {
-		/*
-		this.app = new ParrotAppServ();
-        DocumentReader ontologyWrapper = new JenaOWLReader();
-        DocumentReader ruleWrapper = new RifXmlReader(ontologyWrapper);
-        this.app.setOntologyWrapper(ontologyWrapper);
-        this.app.setRuleWrapper(ruleWrapper);
-        this.template = Thread.currentThread().getContextClassLoader().getResourceAsStream(template);
-        */
+		
 	}
 	
-	// TODO
 	public String exec(String input){
+		app = new ParrotAppServ();
+        DocumentReader ontologyWrapper = new JenaOWLReader();
+        DocumentReader ruleWrapper = new RifXmlReader(ontologyWrapper);
+        app.setOntologyWrapper(ontologyWrapper);
+        app.setRuleWrapper(ruleWrapper);
+        template = Thread.currentThread().getContextClassLoader().getResourceAsStream("html/template.vm");
 		// To debug stream
 		System.out.println("Parrot Core Input:" + input);
-		return "<html><body>Handler Test</body></html>";
+		
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		DocumentaryProject dp = new DocumentaryProject(template, outputStream, "EN");
+		dp.addInput(new StringInput(input, "application/rdf+xml"));
+		
+		try {
+			app.createDocumentation(dp);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return outputStream.toString();
 	}
 		
 }
