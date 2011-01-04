cc334f9255c0011eba0a2ea6079d7e1ec6d86fcd
Diego Berrueta <diego.berrueta@fundacionctic.org>
2011-01-04 10:25:12 +0100

    Changed to the new parrot-core API

--- src/es/ctic/parrot/parrot_eclipse/utils/ParrotCoreWrapper.java
+++ src/es/ctic/parrot/parrot_eclipse/utils/ParrotCoreWrapper.java
@@ -3,13 +3,16 @@ package es.ctic.parrot.parrot_eclipse.utils;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 
 
 import es.ctic.parrot.appserv.ParrotAppServ;
+import es.ctic.parrot.generators.HtmlOutputGenerator;
 import es.ctic.parrot.reader.DocumentReader;
+import es.ctic.parrot.reader.ReaderException;
 import es.ctic.parrot.reader.StringInput;
 import es.ctic.parrot.reader.jena.JenaOWLReader;
-import es.ctic.parrot.reader.naiverifxml.RifXmlReader;
+import es.ctic.parrot.reader.rifle.RifleXmlReader;
 import es.ctic.parrot.DocumentaryProject;
 
 
@@ -25,7 +28,7 @@ public class ParrotCoreWrapper {
 	public String exec(String input, String contenttype){
 		app = new ParrotAppServ();
         DocumentReader ontologyWrapper = new JenaOWLReader();
-        DocumentReader ruleWrapper = new RifXmlReader(ontologyWrapper);
+        DocumentReader ruleWrapper = new RifleXmlReader(ontologyWrapper);
         app.setOntologyWrapper(ontologyWrapper);
         app.setRuleWrapper(ruleWrapper);
         template = Thread.currentThread().getContextClassLoader().getResourceAsStream("html/template.vm");
@@ -33,11 +36,14 @@ public class ParrotCoreWrapper {
 		System.out.println("Parrot Core Input:" + input);
 		
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		DocumentaryProject dp = new DocumentaryProject(template, outputStream, "EN");
+		DocumentaryProject dp = new DocumentaryProject(Locale.ENGLISH);
+		HtmlOutputGenerator generator = new HtmlOutputGenerator(outputStream, template);
 		dp.addInput(new StringInput(input, contenttype));
 		
 		try {
-			app.createDocumentation(dp);
+			app.createDocumentation(dp, generator);
+		} catch (ReaderException e) {
+			throw new RuntimeException(e);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
